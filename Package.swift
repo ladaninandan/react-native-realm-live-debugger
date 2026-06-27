c74558144b93624c0c313010de2cbe33ef896d0a
Nandan Ladani <ladaninandan46@example.com>
2026-06-27 23:51:12 +0530

    Configure Swift Package Manager for iOS client

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version: 5.8
+import PackageDescription
+
+let package = Package(
+    name: "RealmLiveDebugger",
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v10_15)
+    ],
+    products: [
+        .library(
+            name: "RealmLiveDebugger",
+            targets: ["RealmLiveDebugger"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/realm/realm-swift.git", from: "10.0.0")
+    ],
+    targets: [
+        .target(
+            name: "RealmLiveDebugger",
+            dependencies: [
+                .product(name: "RealmSwift", package: "realm-swift")
+            ],
+            path: "client/ios"
+        )
+    ]
+)
